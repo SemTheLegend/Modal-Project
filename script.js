69997c5396e29b8547f311d26fa88878e9962234
SemTheLegend <semmathe@gmail.com>
2023-08-08 18:24:54 +0200

    refactored the code to standard

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 "use strict";
 
-const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
+const modal = document.querySelector(".modal");
 const btnShowModal = document.querySelectorAll(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
 
+// Handles opening the modal.
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+// Handles closing the modal.
 const closeModal = () => {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 }
 
-const openModal = () => {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-}
 
 for (let i = 0; i < btnShowModal.length; i++) {
   btnShowModal[i].addEventListener("click", openModal);
 }
 
-// Hiding the modal when clicking the close btn.
+// For closing both overlay and button to close the modal.
 btnCloseModal.addEventListener("click", closeModal);
-
-// Hiding the modal when clicking the overlay.
 overlay.addEventListener("click", closeModal);
 
-// Listening for keyboard events.
+// Handling Keyboard events.
 document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
-  }
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
 });
